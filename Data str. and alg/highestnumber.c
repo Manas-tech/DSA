b93f40dfae7356a2b01740e9e67829b4c0f7ab9f
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-08-17 00:39:36 +0530

    Add files via upload

---
+++ Data str. and alg/highestnumber.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main(){
+int a[1000],b,q,j,i,r;
+printf("enter the size of array");
+scanf("%d",&b);
+for ( i = 0; i <b ;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("array is ");
+for ( i = 0; i <b ;i++)
+{
+printf(" %d",a[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
