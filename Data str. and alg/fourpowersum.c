b16d11d793a1156438e8a3bfd8ed6b20d18040d0
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-09-04 20:16:29 +0530

    Update fourpowersum.c

--- Data str. and alg/fourpowersum.c
+++ Data str. and alg/fourpowersum.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int a,sum=0,i;
-    printf("enter the number");
+    printf("enter the  number");
     scanf("%d",&a);
     for(i=1;i<=a;i++){
         int b=i*i*i*i;
