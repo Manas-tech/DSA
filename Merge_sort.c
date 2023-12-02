ed5fa48945b4911fea61df4ecc37dca6a27d192e
Harshal Jain <harshaljn04@gmail.com>
2023-12-02 21:27:25 +0530

    Add files via upload

---
+++ Merge_sort.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void print_array(int* a[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+void mergesort()
+{
+    
+}
+int main()
+{
+    int n;
+    scnaf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+}
